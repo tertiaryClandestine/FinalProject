5287bce84473e81eff9c066b10c78d97591ffd82
Joshua Ortmann <joshuaortmann@gmail.com>
2024-11-24 19:10:38 -0600

    Adjust working directory

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -1,25 +1,36 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <iomanip>
 #include "Map.h"
 #include "Player.h"
 
-void DisplayMenu() {
-    // std::ostringstream outSS;
-    std::cout << "------------------------TitleMenu------------------------" << std::endl;
-    std::cout << "----------------------1. Continue------------------------" << std::endl;
-    std::cout << "------------------------2. Load--------------------------" << std::endl;
-    std::cout << "------------------------3. New---------------------------" << std::endl;
-    std::cout << "------------------------4. Quit--------------------------" << std::endl;
-//    std::cout << outSS;
-}
+//std::string centerAlign(std::string str, int width) {
+//    std::stringstream outSS;
+//    int padding = (width / 2) - (str.length() / 2);
+//    outSS <<
+//}
 
+void DisplayMainMenu() {
+    std::cout << "-------DungeonMaster-------" << std::endl;
+    std::cout << "1. Continue" << std::endl;
+    std::cout << "2. Load" << std::endl;
+    std::cout << "3. New" << std::endl;
+    std::cout << "4. Quit" << std::endl;
+}
 
 int main(int argc, const char * argv[]) {
     
+
+//    DisplayMainMenu();
+    
+//        std::string mapFileName = "/Users/joshuaortmann/cosc1437/Projects/FinalProject/FinalProject/data/map.txt";
+    std::string mapFileName = "data/map.txt";
+    
+    
     Map gameMap;
     try {
-        gameMap.loadMap("/Users/joshuaortmann/cosc1437/Projects/FinalProject/FinalProject/data/map.txt");
+        gameMap.loadMap(mapFileName);
     }
     catch (MapFileReadError e) {
         std::cout << "Error Message: " << e.getMessage() << std::endl;
@@ -27,40 +38,40 @@ int main(int argc, const char * argv[]) {
     catch (TileInitializationError e) {
         std::cout << "Error Message: " << e.getMessage() << std::endl;
     }
-  
-    Player pl(&gameMap, 48, 1);
-    
-    gameMap.draw(pl.getLoc());
-    std::cout << std::endl << std::endl << std::endl;
-    char directionInput;
-    std::cout << "Movement options: a=left, w=up, s=down, d=right" << std::endl;
-    std::cout << "pl.x: " << pl.getLoc()->getX() << std::endl;
-    std::cout << "pl.y: " << pl.getLoc()->getY() << std::endl;
-    std::cin >> directionInput;
-    while (directionInput != 'q') {
-        switch (directionInput) {
-            case 'w':
-                pl.move(-1, 0);
-                break;
-            case 's':
-                pl.move(1, 0);
-                break;
-            case 'd':
-                pl.move(0, 1);
-                break;
-            case 'a':
-                pl.move(0, -1);
-                break;
-            default:
-                break;
-        }
-//        pl.move(gameMap.getTile(pos_x, pos_y));
-        gameMap.draw(pl.getLoc());
-        std::cout << std::endl << std::endl << std::endl;
-        std::cout << "Movement options: a=left, w=up, s=down, d=right" << std::endl;
-        std::cout << "pl.x: " << pl.getLoc()->getX() << std::endl;
-        std::cout << "pl.y: " << pl.getLoc()->getY() << std::endl;
-        std::cin >> directionInput;
-    }
+//  
+//    Player pl(&gameMap, 48, 1);
+//    
+//    gameMap.draw(pl.getLoc());
+//    std::cout << std::endl << std::endl << std::endl;
+//    char directionInput;
+//    std::cout << "Movement options: a=left, w=up, s=down, d=right" << std::endl;
+//    std::cout << "pl.x: " << pl.getLoc()->getX() << std::endl;
+//    std::cout << "pl.y: " << pl.getLoc()->getY() << std::endl;
+//    std::cin >> directionInput;
+//    while (directionInput != 'q') {
+//        switch (directionInput) {
+//            case 'w':
+//                pl.move(-1, 0);
+//                break;
+//            case 's':
+//                pl.move(1, 0);
+//                break;
+//            case 'd':
+//                pl.move(0, 1);
+//                break;
+//            case 'a':
+//                pl.move(0, -1);
+//                break;
+//            default:
+//                break;
+//        }
+////        pl.move(gameMap.getTile(pos_x, pos_y));
+//        gameMap.draw(pl.getLoc());
+//        std::cout << std::endl << std::endl << std::endl;
+//        std::cout << "Movement options: a=left, w=up, s=down, d=right" << std::endl;
+//        std::cout << "pl.x: " << pl.getLoc()->getX() << std::endl;
+//        std::cout << "pl.y: " << pl.getLoc()->getY() << std::endl;
+//        std::cin >> directionInput;
+//    }
     
 }
